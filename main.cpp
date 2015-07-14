a79dba97da85ad747e8b009295d24a4e3ded57cb
valseek <valseek@amlab-linux>
2015-07-14 14:29:05 +0800

    an opengl-teapot
    Signed-off-by: valseek <valseek@amlab-linux>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,43 @@
+#include <GL/glut.h>
 #include <iostream>
-
 using namespace std;
 
 
+int Count = 0;
 
-int main(){
-    cout << "Hello world!" << endl;
+
+
+void init(){
+    cout << ++Count << endl;
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    glOrtho(-5, 5, -5, 5, 5, 15);
+    glMatrixMode(GL_MODELVIEW);
+    gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0);
+}
+
+void display(){
+    cout << ++Count << endl;
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 0, 0);
+    glutWireTeapot(3);
+    glFlush();
+}
+
+
+int main(int argc, char* argv[]){
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+    glutInitWindowPosition(0, 0);
+    glutInitWindowSize(300, 300);
+    glutCreateWindow("OpenGL 3D View");
+
+    init();
+    glutDisplayFunc(display);
+    glutMainLoop();
+
     return 0;
 }
+
+
+
