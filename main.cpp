ba468bacc0f90224f9136601f81389c55d14057a
valseek <valseek@amlab-linux>
2015-07-16 19:13:30 +0800

    a new workable framework
    Signed-off-by: valseek <valseek@amlab-linux>

--- main.cpp
+++ main.cpp
@@ -1,43 +1,95 @@
 #include <GL/glut.h>
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
-
-int Count = 0;
-
+static GLfloat spin = 0.0;
 
 
-void init(){
-    cout << ++Count << endl;
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    glMatrixMode(GL_PROJECTION);
-    glOrtho(-5, 5, -5, 5, 5, 15);
-    glMatrixMode(GL_MODELVIEW);
-    gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0);
-}
 
-void display(){
-    cout << ++Count << endl;
+
+void display(void){
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0, 0, 0);
-    glutWireTeapot(3);
-    glFlush();
+    glPushMatrix();
+    glRotatef(spin, 0.0, 0.0, 1.0);
+    glColor3f(1.0, 1.0, 1.0);
+    glRectf(-25.0, -25.0, 25.0, 25.0);
+    glPopMatrix();
+    glutSwapBuffers();
+}
+
+void spinDisplay(void){
+    spin = spin + 2.0;
+    if (spin > 360.0)
+        spin = spin - 360.0;
+    glutPostRedisplay();
+}
+
+void init(void){
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_SMOOTH);
+}
+
+void reshape(int w, int h){
+    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void mouse(int button, int state, int x, int y){
+    switch (button) {
+        case GLUT_LEFT_BUTTON:
+            if (state == GLUT_DOWN)
+            glutIdleFunc(spinDisplay);
+            break;
+        case GLUT_MIDDLE_BUTTON:
+            break;
+        case GLUT_RIGHT_BUTTON:
+            if (state == GLUT_DOWN)
+                glutIdleFunc(NULL);
+            break;
+        default:
+            break;
+    }
+}
+void keyboard(unsigned char key,int x,int y){
+    switch(key){
+        case 'a':
+            break;
+        case 'b':
+            break;
+        default:
+            break;
+    }
 }
 
 
-int main(int argc, char* argv[]){
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-    glutInitWindowPosition(0, 0);
-    glutInitWindowSize(300, 300);
-    glutCreateWindow("OpenGL 3D View");
-
-    init();
-    glutDisplayFunc(display);
-    glutMainLoop();
-
-    return 0;
+int main(int argc, char** argv)
+{
+   glutInit(&argc, argv);
+   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+   glutInitWindowSize (500, 500);
+   glutInitWindowPosition (100, 100);
+   glutCreateWindow (argv[0]);
+   init ();
+   glutDisplayFunc(display);
+   glutReshapeFunc(reshape);
+   glutMouseFunc(mouse);
+   glutKeyboardFunc(keyboard);
+   glutMainLoop();
+   return 0;
 }
+
+
+
+
+
+
+
+
 
 
 
