be96658f4d002f4fb94f366d20c4babfeb5a81cc
valseek <valseek@amlab-linux>
2015-07-16 21:58:02 +0800

    xyz axis finished.(need to make it look better)
    Signed-off-by: valseek <valseek@amlab-linux>

--- main.cpp
+++ main.cpp
@@ -6,11 +6,30 @@ using namespace std;
 static GLfloat spin = 0.0;
 
 
-void typex(){
-
+void typex(float L1){
+    glColor3f(0.0, 1.0, 0.0);
+    glLineWidth(2.0);
+    glBegin(GL_LINES);
+        glVertex3f(0.0, L1, L1);
+        glVertex3f(0.0, -L1, -L1);
+    glEnd();
+    glBegin(GL_LINES);
+        glVertex3f(0.0, L1, -L1);
+        glVertex3f(0.0, -L1, L1);
+    glEnd();
 }
-void typey(){
-
+void typey(float L1){
+    glColor3f(0.0, 1.0, 0.0);
+    glLineWidth(2.0);
+    glBegin(GL_LINE_STRIP);
+        glVertex3f(-L1, 0.0, -L1);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(L1, 0.0, -L1);
+    glEnd();
+    glBegin(GL_LINES);
+        glVertex3f(0.0, 0.0, 0.0);
+        glVertex3f(0.0, 0.0, L1);
+    glEnd();
 }
 void typez(float L1){
     glColor3f(0.0, 1.0, 0.0);
@@ -23,29 +42,41 @@ void typez(float L1){
     glEnd();
 }
 void xyzAxis(void){
-    glColor3f(1.0, 1.0, 1.0);
-    glLineWidth(2.0);
 
     //x axis
+    glColor3f(1.0, 1.0, 1.0);
+    glLineWidth(2.0);
     glBegin(GL_LINES);
         glVertex3f(-15.0, 0.0, 0.0);
         glVertex3f(40.0, 0.0, 0.0);
     glEnd();
+    glPushMatrix();
+        glTranslatef(40.0, 0.0, 0.0);
+        typex(3.0);
+    glPopMatrix();
 
     //y axis
+    glColor3f(1.0, 1.0, 1.0);
+    glLineWidth(2.0);
     glBegin(GL_LINES);
         glVertex3f(0.0, -15.0, 0.0);
         glVertex3f(0.0, 40.0, 0.0);
     glEnd();
+    glPushMatrix();
+        glTranslatef(0.0, 40.0, 0.0);
+        typey(3.0);
+    glPopMatrix();
 
     //z axis
+    glColor3f(1.0, 1.0, 1.0);
+    glLineWidth(2.0);
     glBegin(GL_LINES);
         glVertex3f(0.0, 0.0, -15.0);
         glVertex3f(0.0, 0.0, 40.0);
     glEnd();
     glPushMatrix();
         glTranslatef(0.0, 0.0, 40.0);
-        typez(1.0);
+        typez(3.0);
     glPopMatrix();
 }
 
