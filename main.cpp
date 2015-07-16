9bf8f2d3248aa820f60289e1dff455ac2dd7f453
valseek <valseek@amlab-linux>
2015-07-16 21:48:53 +0800

    xyz axis , z axis already done.
    Signed-off-by: valseek <valseek@amlab-linux>

--- main.cpp
+++ main.cpp
@@ -6,26 +6,55 @@ using namespace std;
 static GLfloat spin = 0.0;
 
 
+void typex(){
 
+}
+void typey(){
+
+}
+void typez(float L1){
+    glColor3f(0.0, 1.0, 0.0);
+    glLineWidth(2.0);
+    glBegin(GL_LINE_STRIP);
+        glVertex3f(-L1, L1, 0.0);
+        glVertex3f(L1, L1, 0.0);
+        glVertex3f(-L1, -L1, 0.0);
+        glVertex3f(L1, -L1, 0.0);
+    glEnd();
+}
+void xyzAxis(void){
+    glColor3f(1.0, 1.0, 1.0);
+    glLineWidth(2.0);
+
+    //x axis
+    glBegin(GL_LINES);
+        glVertex3f(-15.0, 0.0, 0.0);
+        glVertex3f(40.0, 0.0, 0.0);
+    glEnd();
+
+    //y axis
+    glBegin(GL_LINES);
+        glVertex3f(0.0, -15.0, 0.0);
+        glVertex3f(0.0, 40.0, 0.0);
+    glEnd();
+
+    //z axis
+    glBegin(GL_LINES);
+        glVertex3f(0.0, 0.0, -15.0);
+        glVertex3f(0.0, 0.0, 40.0);
+    glEnd();
+    glPushMatrix();
+        glTranslatef(0.0, 0.0, 40.0);
+        typez(1.0);
+    glPopMatrix();
+}
 
 void display(void){
     glClear(GL_COLOR_BUFFER_BIT);
     glPushMatrix();
     glRotatef(spin, 1.0, 1.0, 1.0);
 
-    glColor3f(1.0, 1.0, 1.0);
-    glBegin(GL_LINES);
-        glVertex3f(-15.0, 0.0, 0.0);
-        glVertex3f(40.0, 0.0, 0.0);
-    glEnd();
-    glBegin(GL_LINES);
-        glVertex3f(0.0, -15.0, 0.0);
-        glVertex3f(0.0, 40.0, 0.0);
-    glEnd();
-    glBegin(GL_LINES);
-        glVertex3f(0.0, 0.0, -15.0);
-        glVertex3f(0.0, 0.0, 40.0);
-    glEnd();
+    xyzAxis();
 
     glPopMatrix();
     glutSwapBuffers();
@@ -47,7 +76,7 @@ void reshape(int w, int h){
     glViewport (0, 0, (GLsizei) w, (GLsizei) h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(-50.0, 50.0, -50.0, 50.0, 1.0, 99.0);
+    glOrtho(-50.0, 50.0, -50.0, 50.0, 1.0, 101.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(0.0, 0.0, 51.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
